512ac07b446d7fa785e5d55713dcfd68d6324058
Luquian Cesar <luquian.santos@outlook.com>
2024-11-27 02:33:09 -0300

    fix: Recrusos de Cartão

--- mscartoes/src/main/java/io/github/lukecesar7/mscartoes/application/CartoesResource.java
+++ mscartoes/src/main/java/io/github/lukecesar7/mscartoes/application/CartoesResource.java
@@ -5,10 +5,9 @@ import io.github.lukecesar7.mscartoes.domain.Cartao;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("cartoes")
@@ -27,5 +26,10 @@ public class CartoesResource {
         service.save(cartao);
         return ResponseEntity.status(HttpStatus.CREATED).build();
 
+    }
+    @GetMapping(params = "renda")
+    public ResponseEntity<List<Cartao>> getCartoesRendaAte(@RequestParam("renda") Long renda){
+        List<Cartao> list = service.getCartoesRendaMenorIgual(renda);
+        return ResponseEntity.ok(list);
     }
 }
